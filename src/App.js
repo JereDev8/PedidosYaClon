71c02b1f28afc52c9c1336b1a98d76a23ce0e005
Jeremias Quinteros <jeremias8quinteros@gmail.com>
2023-02-28 16:04:59 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import { useEffect } from 'react';
+import './App.css';
+import Interfaz from './components/interfaz';
+import Menu from './components/menu';
+
+function App() {
+
+  
+
+  return (
+    <div className="App">
+      <Menu/>
+      <Interfaz/>
+    </div>
+  );
+}
+
+export default App;
